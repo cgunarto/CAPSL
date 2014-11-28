ec03422c62ebad4c9eac9e79d8d1bd9eadb2f1f7
Christina Gunarto <cgunarto@gmail.com>
2014-11-28 13:12:58 -0800

    started profile vc and added container views

---
+++ Capsl/CapslrContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  CapslrContactViewController.h
+//  Capsl
+//
+//  Created by CHRISTINA GUNARTO on 11/28/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CapslrContactViewController : UIViewController
+
+@end
---
+++ Capsl/CapslrContactViewController.m
@@ -0,0 +1,24 @@
+//
+//  CapslrContactViewController.m
+//  Capsl
+//
+//  Created by CHRISTINA GUNARTO on 11/28/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import "CapslrContactViewController.h"
+
+@interface CapslrContactViewController ()
+
+@end
+
+@implementation CapslrContactViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
