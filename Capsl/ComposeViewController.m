2c3ae586c92eee1bc855ec79f638781ac5434f58
Christina Gunarto <cgunarto@gmail.com>
2014-11-28 19:02:13 -0800

    added picking date and hour but still has bug

--- Capsl/ComposeViewController.m
+++ Capsl/ComposeViewController.m
@@ -15,6 +15,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *deliveryTimeTextField;
 @property (weak, nonatomic) IBOutlet UITextField *recipientTextfield;
 
+@property UIDatePicker *datePicker;
+@property UIDatePicker *timePicker;
+
 @end
 
 @implementation ComposeViewController
@@ -24,27 +27,71 @@
     [super viewDidLoad];
     self.imageView.image = self.chosenImage;
     self.deliveryDateTextField.delegate = self;
+    self.deliveryTimeTextField.delegate = self;
 
-    UIDatePicker *datePicker = [[UIDatePicker alloc]init];
+    //Setting the date picker for Delivery Date Textfield
+    self.datePicker = [[UIDatePicker alloc]init];
+    self.datePicker.datePickerMode = UIDatePickerModeDate;
+    [self.datePicker setDate:[NSDate date]];
 
-    datePicker.datePickerMode = UIDatePickerModeDate;
-    [datePicker setDate:[NSDate date]];
-    [datePicker addTarget:self
-                   action:@selector(updateTextField:)
+    //set minimum Date as right now
+    [self.datePicker setMinimumDate:[NSDate date]];
+    [self.datePicker addTarget:self
+                   action:@selector(updateDateTextField:)
          forControlEvents:UIControlEventValueChanged];
 
-    [self.deliveryDateTextField setInputView:datePicker];
+    //setting keyboard as datePicker
+    [self.deliveryDateTextField setInputView:self.datePicker];
 
-    [datePicker addTarget:self
-                   action:@selector(updateTextField:)
+    //Setting the date picker for the Delivery Time Textfield
+    self.timePicker = [[UIDatePicker alloc]init];
+    self.timePicker.datePickerMode = UIDatePickerModeTime;
+    [self.timePicker setDate:self.datePicker.date];
+    [self.timePicker addTarget:self
+                   action:@selector(updateTimeTextField:)
          forControlEvents:UIControlEventValueChanged];
+
+    [self.deliveryTimeTextField setInputView:self.timePicker];
+
 
 }
 
--(void)updateTextField:(id)sender
+-(void)updateDateTextField:(id)sender
 {
+    NSLog(@"textfield value changed");
     UIDatePicker *picker = (UIDatePicker*)self.deliveryDateTextField.inputView;
-    self.deliveryDateTextField.text = [NSString stringWithFormat:@"%@",picker.date];
+
+    //Setting a minimum and maximum date for TimeTextField that is 24 hours of the chosen date from DateTextField
+//    NSDate *now = self.datePicker.date;
+//    int daysToAdd = 2;
+//    NSDate *aDayFromNow = [now dateByAddingTimeInterval:60*60*24*daysToAdd];
+
+    [self.timePicker setDate:self.datePicker.date];
+//    [self.timePicker setMinimumDate:self.datePicker.date];
+//    [self.timePicker setMaximumDate:aDayFromNow];
+
+    NSDate *pickerDate = picker.date;
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"dd-MM-yyyy"];
+    NSString *dateString = [dateFormat stringFromDate:pickerDate];
+    NSLog(@"%@", dateString);
+
+    //TODO: use NSDateFormatter
+    self.deliveryDateTextField.text = [NSString stringWithFormat:@"%@",dateString];
+}
+
+-(void)updateTimeTextField:(id)sender
+{
+    UIDatePicker *picker = (UIDatePicker*)self.deliveryTimeTextField.inputView;
+//    self.deliveryTimeTextField.text = [NSString stringWithFormat:@"%@",picker.date];
+
+    NSDate *pickerDate = picker.date;
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"dd-MM-yyyy HH:mm"];
+    NSString *dateString = [dateFormat stringFromDate:pickerDate];
+    NSLog(@"%@", dateString);
+
+    self.deliveryTimeTextField.text = [NSString stringWithFormat:@"%@",dateString];
 }
 
 
