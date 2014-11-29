d37536144ad8595f3b86339138a3898c95eeb98b
adhsu101 <mm@Alexs-MacBook-Pro-2.local>
2014-11-29 13:13:03 -0800

    porting timeline vcs

---
+++ Capsl/JCATimelineViewController.h
@@ -0,0 +1,27 @@
+//
+//  JCAScrubberViewController.h
+//  Timeline Test 2
+//
+//  Created by Mobile Making on 11/24/14.
+//  Copyright (c) 2014 Alex Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol TimelineDelegate <NSObject>
+
+- (void)indexPathForTimelineCellAtCenter:(NSIndexPath *)indexPath fromTap:(BOOL)didTap;
+
+- (void)timelineDidEndScrolling;
+
+@end
+
+@interface JCATimelineViewController : UIViewController
+
+- (void)centerCorrespondingMonthCell:(NSIndexPath *)indexPath;
+
+@property (nonatomic, weak) id <TimelineDelegate> delegate;
+
+@end
+
+
