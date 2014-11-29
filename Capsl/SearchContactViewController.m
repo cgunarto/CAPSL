d8a35e48f5ca2b1c56daddbeb491e69283d66e36
Christina Gunarto <cgunarto@gmail.com>
2014-11-29 15:29:41 -0800

    added code for search contact VC and cell identifier cell

--- Capsl/SearchContactViewController.m
+++ Capsl/SearchContactViewController.m
@@ -7,10 +7,17 @@
 //
 
 #import "SearchContactViewController.h"
+#import "Contact.h"
+#import "Capslr.h"
+#import "AllContactTableViewCell.h"
 
 @interface SearchContactViewController () <UITableViewDataSource, UITableViewDelegate, UISearchDisplayDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@property (nonatomic, strong) NSArray *capslrArray;
+@property (nonatomic, strong) NSArray *contactsArray;
+
 
 @end
 
@@ -19,16 +26,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    [Contact retrieveAllContactsWithBlock:^(NSArray *contacts)
+     {
+         self.contactsArray = [contacts mutableCopy];
+
+         [Capslr returnCapslrWithContactsArray:self.contactsArray withCompletion:^(NSArray *capslrObjectsArray, NSError *error)
+          {
+              self.capslrArray = capslrObjectsArray;
+              [self.tableView reloadData];
+
+          }];
+         
+     }];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    AllContactTableViewCell *allContactCell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+
+    Capslr *capslr = self.capslrArray[indexPath.row];
+    allContactCell.nameLabel.text = capslr.name;
+    allContactCell.usernameLabel.text = capslr.username;
+    allContactCell.phoneLabel.text = capslr.phone;
+
+    return allContactCell;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return self.capslrArray.count;
 }
 
 @end
