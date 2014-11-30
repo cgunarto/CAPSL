53e9ee952dfec13162a87d04a926b3bfc4adc00d
Christina Gunarto <cgunarto@gmail.com>
2014-11-29 23:48:21 -0800

    consolidated my folder of VCs and added audio VC

---
+++ Capsl/RecordAudioViewController.m
@@ -0,0 +1,24 @@
+//
+//  RecordAudioViewController.m
+//  Capsl
+//
+//  Created by CHRISTINA GUNARTO on 11/29/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import "RecordAudioViewController.h"
+
+@interface RecordAudioViewController ()
+
+@end
+
+@implementation RecordAudioViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+
+@end
