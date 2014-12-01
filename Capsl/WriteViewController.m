4dda5fed0b6a9f4ea2e83ac3b220f257b1ee7b04
Christina Gunarto <cgunarto@gmail.com>
2014-12-01 14:00:32 -0800

    sending text works

--- Capsl/WriteViewController.m
+++ Capsl/WriteViewController.m
@@ -27,21 +27,57 @@
 {
     [super viewDidLoad];
 
+
     self.textView.delegate = self;
+
+    //Setting CPSL sender
+    [Capslr returnCapslrFromPFUser:[PFUser currentUser] withCompletion:^(Capslr *currentCapslr, NSError *error)
+     {
+         self.createdCapsl.sender = currentCapslr;
+     }];
+
+    //Initializing Capsl object and its type
+    self.createdCapsl = [Capsl object];
+    self.createdCapsl.type = @"text";
 }
 
 - (IBAction)onNextButtonPressed:(UIButton *)sender
 {
-    [self performSegueWithIdentifier:@"segueToContactSearch" sender:self];
+    if (![self.textView.text isEqualToString:@""])
+    {
+        //TODO:ask for user confirmation before moving forward
+        [self performSegueWithIdentifier:@"segueToContactSearch" sender:self.nextButton];
+    }
+
+    else
+    {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"MESSAGE EMPTY"
+                                                                       message:@"Please write a message"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:nil];
+        [alert addAction:okButton];
+        [self presentViewController:alert
+                           animated:YES
+                         completion:nil];
+
+    }
 }
 
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([sender isEqual:self.nextButton])
     {
         SearchContactViewController *searchContactVC = segue.destinationViewController;
         searchContactVC.createdCapsl = self.createdCapsl;
     }
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView
+{
+    self.createdCapsl.text = textView.text;
 }
 
 
