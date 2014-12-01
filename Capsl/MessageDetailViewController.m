c6b03e073b230e9a738e83baf8a954313574b495
Jonathan Kim <jonnokim17@gmail.com>
2014-11-30 17:10:57 -0800

    sender data passed

--- Capsl/MessageDetailViewController.m
+++ Capsl/MessageDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MessageDetailViewController.h"
+#import "Capslr.h"
 
 @interface MessageDetailViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *senderLabel;
@@ -21,12 +22,21 @@
 {
     [super viewWillAppear:animated];
 
+    NSLog(@"%@", self.chosenCapsl);
 
+    // Passing Delivery Date
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"MM-dd-yyyy"];
     NSDate *deliveryDate = self.chosenCapsl.deliveryTime;
-
     self.deliveryDateLabel.text = [dateFormatter stringFromDate:deliveryDate];
+
+    //Sender
+    PFQuery *query = [Capslr query];
+    [query whereKey:@"objectId" equalTo: self.chosenCapsl.sender.objectId];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+
+        self.senderLabel.text = [NSString stringWithFormat:@"From: %@", object[@"username"]];
+    }];
 
 }
 
