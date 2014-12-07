1d4457713be0f1b3552069736cafbc0875694d35
Jonathan Kim <jonnokim17@gmail.com>
2014-12-06 21:38:22 -0800

    need to fix bug

--- Capsl/RootViewController.m
+++ Capsl/RootViewController.m
@@ -34,26 +34,26 @@
 
     // Check to see if user quit before entering verification code
 
-    if ([PFUser currentUser])
-    {
-        [Capslr returnCapslrFromPFUser:[PFUser currentUser] withCompletion:^(Capslr *currentCapslr, NSError *error) {
-            if (!currentCapslr.name)
-            {
-                [currentCapslr deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                    if (!error)
-                    {
-                        [[PFUser currentUser] deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                            if (!error)
-                            {
-                                [PFUser logOut];
-                                [self manageLogin];
-                            }
-                        }];
-                    }
-                }];
-            }
-        }];
-    }
+//    if ([PFUser currentUser])
+//    {
+//        [Capslr returnCapslrFromPFUser:[PFUser currentUser] withCompletion:^(Capslr *currentCapslr, NSError *error) {
+//            if (!currentCapslr.name)
+//            {
+//                [currentCapslr deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//                    if (!error)
+//                    {
+//                        [[PFUser currentUser] deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//                            if (!error)
+//                            {
+//                                [PFUser logOut];
+//                                [self manageLogin];
+//                            }
+//                        }];
+//                    }
+//                }];
+//            }
+//        }];
+//    }
 
 
     self.sendCapsuleButton.alpha = 0;
