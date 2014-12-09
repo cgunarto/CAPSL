6451e89888b2c583fe3b513e8761ac7b13beb76b
adhsu101 <mm@Alexs-MacBook-Pro-2.local>
2014-12-09 12:51:41 -0800

    background resizing

--- Capsl/BackgroundGenerator.m
+++ Capsl/BackgroundGenerator.m
@@ -8,20 +8,20 @@
 
 #import "BackgroundGenerator.h"
 #import "UIImage+ImageEffects.h"
+#import "UIImage+Resize.h"
 
 @implementation BackgroundGenerator
 
 + (UIImage *)blurImage:(UIImage *)image
 {
 
-    [self imageResize:image];
+    UIImage *newImage = [self imageResize:image];
 
-//    UIColor *tintColor = [UIColor colorWithWhite:0.3 alpha:0.1];
-//    UIImage *blurredImage = [image applyBlurWithRadius:20 tintColor:tintColor saturationDeltaFactor:0.8 maskImage:nil];
+    UIColor *tintColor = [UIColor colorWithWhite:0.3 alpha:0.1];
+    UIImage *blurredImage = [newImage applyBlurWithRadius:20 tintColor:tintColor saturationDeltaFactor:0.8 maskImage:nil];
 
-//    return blurredImage;
+    return blurredImage;
 
-    return image;
 }
 
 + (UIImage *)imageResize :(UIImage *)img
@@ -31,10 +31,14 @@
     CGSize newSize = [[UIScreen mainScreen] bounds].size;
 
     //UIGraphicsBeginImageContext(newSize);
-    UIGraphicsBeginImageContextWithOptions(newSize, NO, scale);
-    [img drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
-    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+//    UIGraphicsBeginImageContextWithOptions(newSize, NO, scale);
+//    [img drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+//    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+
+    UIImage *newImage = [img resizedImageWithContentMode:UIViewContentModeScaleAspectFill bounds:newSize interpolationQuality:0.0];
+
+
     return newImage;
 }
 
