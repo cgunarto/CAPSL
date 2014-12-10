ce737e89b27cc5e1bf4a9b8f5212171f980f1d97
Jonathan Kim <jonnokim17@gmail.com>
2014-12-10 08:43:06 -0800

    updated

--- Capsl/JKCountDownTimer.m
+++ Capsl/JKCountDownTimer.m
@@ -154,26 +154,6 @@
 }
 
 
-- (NSTimeInterval)setupTimer
-{
-    NSCalendarUnit dayOfWeek = NSCalendarUnitWeekday;
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-    NSInteger todaysDay = [calendar component:dayOfWeek fromDate:[NSDate date]];
-
-    NSInteger daysUntilSunday = 8 - todaysDay;
-    NSTimeInterval timeFromStartOfTodayThroughSaturday = kDayInSeconds * daysUntilSunday;
-    NSTimeInterval timeFromStartOfTodayThroughTomorrow = kDayInSeconds * 2;
-
-    NSDate *startOfToday = [calendar startOfDayForDate:[NSDate date]];
-
-    NSTimeInterval timeSinceMidnightToday = [startOfToday timeIntervalSinceNow];
-
-    NSTimeInterval timeFromNowUntilSunday = timeSinceMidnightToday + timeFromStartOfTodayThroughSaturday;
-
-    return timeFromNowUntilSunday;
-}
-
-
 //-(instancetype)init
 //{
 //
